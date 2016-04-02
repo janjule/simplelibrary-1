feb605bfd5aa9685bbc26c2903da28a75bdf2e09
janjule <janjule@gmail.com>
2016-04-02 17:37:07 +0200

    Rozsiren anglicky slovnik

--- langs/en.php
+++ langs/en.php
@@ -9,4 +9,32 @@ $translations = [
     "PUBLICATION_ADD_PUBLISHED" => "Published date",
     "PUBLICATION_ADD_SUBMIT"    => "Add",
     "PUBLICATION_ADD_SUCCESS"   => "Publication has been added successfully.",
+
+    "AUTHOR_ADD_HTMLTITLE"      => "Add author",
+    "AUTHOR_ADD_FIRSTNAME"      => "Firstname",
+    "AUTHOR_ADD_LASTNAME"       => "Lastname",
+    "AUTHOR_ADD_BORN"           => "Date of born",
+    "AUTHOR_ADD_DECEASED"       => "Died",
+    "AUTHOR_ADD_NATIONALITY"    => "Nationality",
+    "AUTHOR_ADD_SUBMIT"         => "Add author",
+    "AUTHOR_ADD_SUCCESS"        => "Successfully added :-)",    
+
+    "PUBLISHER_ADD_HTMLTITLE"   => "Add publisher",
+    "PUBLISHER_ADD_TITLE"       => "Title",
+    "PUBLISHER_ADD_ADDRESS"     => "Address",
+    "PUBLISHER_ADD_SUBMIT"      => "Add publisher",
+    "PUBLISHER_ADD_SUCCESS"     => "Successfully added :-)",
+
+    "AGERANGE_ADD_HTMLTITLE"    => "Add agerange",
+    "AGERANGE_ADD_TITLE"        => "Title",
+    "AGERANGE_ADD_YEARSFROM"    => "Years from ",
+    "AGERANGE_ADD_YEARSTO"      => "Yeras to ",
+    "AGERANGE_ADD_SUBMIT"       => "Add agerange",
+    "AGERANGE_ADD_SUCCESS"      => "Successfully added :-)",
+
+    "GENRE_ADD_HTMLTITLE"       => "Add genre",
+    "GENRE_ADD_TITLE"           => "Title",
+    "GENRE_ADD_SUBMIT"          => "Add genre",
+    "GENRE_ADD_SUCCESS"         => "Successfully added :-)",
+
 ];
